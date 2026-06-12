13d17059d8c434ffba22b4a228cdb11f12aaf8a6
DelMedicalTW <mmuqeem@delmedical.com>
2026-06-12 11:07:53 -0500

    rewrite to vite

--- docs/js/config.js
+++
@@ -1,5 +0,0 @@
-var PROXY = 'https://pdf-review-proxy.mmuqeem.workers.dev';
-var STORAGE_KEY = 'delmed-annotations-v2';
-var ISSUE_LABEL_PREFIX = 'pdf-review';
-var OWNER = 'DelMedicalTW';
-var REPO = 'DelMedicalRelease';
